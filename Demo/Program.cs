419d13f2e14868f1fb53d922c49bdc3764331138
Gamgom29 <abdogamgom2015@gmail.com>
2024-07-18 01:06:58 +0300

    Part 01 Operators Overloading - Binary Operators

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace Demo
+﻿using Demo.Operators_Overloading;
+
+namespace Demo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Complex C1 = new Complex() { Real = 2 , Imag = 4};
+            Complex C2 = new Complex() { Real = 3, Imag = 5 };
+
+            Complex C3 = default;
+
+            C3 = C1 + C2;
+            Console.WriteLine($"C1 = {C1}");
+            Console.WriteLine($"C2 = {C2}");
+            Console.WriteLine("----------------");
+            Console.WriteLine($"C3 = {C3}");
+
+            Complex C4 = C1 + C2 + C3;
+
         }
     }
 }
