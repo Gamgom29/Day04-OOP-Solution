7455f8b8210275f6861bb7f1b6ae55b5122a762a
Gamgom29 <abdogamgom2015@gmail.com>
2024-07-18 17:10:42 +0300

    Project01 Q01 Define 3D Pint Class With Constructors

---
+++ Assignment/Program.cs
@@ -0,0 +1,13 @@
+﻿using Assignment.Project01;
+
+namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Project01
+            #endregion
+        }
+    }
+}
---
+++ Assignment/Project01/Point3D.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment.Project01
+{
+    internal class Point3D
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+
+        public Point3D()
+        {
+            
+        }
+        public Point3D(int X , int Y)
+        {
+            this.X = X;
+            this.Y = Y;
+        }
+        public Point3D(int X , int Y , int Z) :this(X,Y)
+        {
+            this.Z = Z;
+        }
+
+        public override string ToString()
+        {
+            return $"({X} , {Y} , {Z})";
+        }
+    }
+}
